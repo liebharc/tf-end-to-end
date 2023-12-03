eed9740ed9de4e1280acb64076eab02a294ebe5b
Christian Liebhardt <christian.liebhardt@arcor.de>
2023-12-03 18:14:30 +0100

    Fixed call to ctc_model.load_ctc_crnn

--- ctc_predict.py
+++ ctc_predict.py
@@ -7,7 +7,7 @@ import numpy as np
 
 # Predict the semantic string of a music score image
 def predict(image_path, model_path, voc_file):
-    sess, input, seq_len, decoded, loss, rnn_keep_prob, width_reduction, height, int2word = ctc_model.load_ctc_crnn(sess, model_path, voc_file)
+    sess, input, seq_len, decoded, loss, rnn_keep_prob, width_reduction, height, int2word = ctc_model.load_ctc_crnn(model_path, voc_file)
 
     image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
     image = ctc_utils.resize(image, height)
